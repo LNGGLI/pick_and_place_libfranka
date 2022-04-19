5f7e42e5abe8b8862440acc36420946d28920091
giulio <giulio.luongo@outlook.it>
2022-04-19 20:12:00 +0200

    Aggiunto collegamento a robot

--- src/TrajAction.cpp
+++ src/TrajAction.cpp
@@ -47,17 +47,17 @@ TrajAction::TrajAction()
   try {
 
     // Connessione al robot
-    // robot_ = std::make_unique<franka::Robot>(robot_IP);
+    robot_ = std::make_unique<franka::Robot>(robot_IP);
 
-    // setDefaultBehavior(*robot_);
+    setDefaultBehavior(*robot_);
 
     state_pub_ = nh_.advertise<sensor_msgs::JointState>("joint_state", 1);
     // start publisher thread
-    // publish_thread_ = std::make_unique<std::thread>(
-    //     std::bind(&TrajAction::publish_state, this));
+    publish_thread_ = std::make_unique<std::thread>(
+        std::bind(&TrajAction::publish_state, this));
 
     // Start as
-    // joint_point_traj_as_.start();
+    joint_point_traj_as_.start();
     joint_traj_as_.start();
 
     std::cout << "Creato oggetto ActionTraj e avviati i server \n";
@@ -75,16 +75,16 @@ void TrajAction::JointTrajCB(
 
   using std::pow;
   pick_and_place_libfranka::JointTrajectoryFeedback joint_traj_feedback;
-  pick_and_place_libfranka::JointTrajectoryResult joint_traj_result_;
+  pick_and_place_libfranka::JointTrajectoryResult joint_traj_result;
 
   try {
 
     robot_mutex_.lock();
 
-    // Interpolation
+    // get initial state and add it to the trajectory
     trajectory_msgs::JointTrajectoryPoint initial_state;
     for (int i = 0; i < 7; i++) {
-      initial_state.positions.push_back(0.0);
+      initial_state.positions.push_back(robot_->readOnce().q[i]);
     }
     initial_state.time_from_start = ros::Duration(0.0);
 
@@ -93,68 +93,72 @@ void TrajAction::JointTrajCB(
     for (int i = 0; i < goal->trajectory.points.size(); i++)
       points.push_back(goal->trajectory.points[i]);
 
-    double tf = points.back().time_from_start.toSec();
-
+    // Interpolation
     std::vector<TooN::Matrix<TooN::Dynamic, 4, double>> coeff =
         compute_polynomial_interpolation(points);
 
-    for (int joint = 0; joint < 7; joint++) {
-      std::cout << "Matrice dei coefficienti giunto " << joint << "\n";
-      for (int i = 0; i < points.size() - 1; i++) {
-        for (int j = 0; j < 4; j++) {
-          std::cout << coeff[joint][i][j] << " ";
-        }
-        std::cout << "\n\n";
-      }
-    }
-
-    ROS_INFO("\nComputed polynomial interpolation\n");
+    double tf =
+        points.back().time_from_start.toSec(); // desired trajectory duration
     joint_traj_feedback.time_left = tf;
     joint_traj_as_.publishFeedback(joint_traj_feedback);
 
+    sensor_msgs::JointState joint_state;
+    joint_state.name = {"j1", "j2", "j3", "j4", "j5", "j6", "j7"};
+
     // Start executing trajectory
     std::array<double, 7> q_command;
-
-    double t = 0;
-    ros::Rate loop_rate(1000);
+    double t = 0.0;
     int p = 0;
-    while (ros::ok() && t < tf) {
+    bool success = false;
 
-      t = t + 0.001;
-      joint_traj_feedback.time_left = tf - t;
-      joint_traj_as_.publishFeedback(joint_traj_feedback);
-
-      // Compute q_command (3rd degree polynomial function)
-      for (int i = 0; i < 7; i++)
-        q_command[i] = coeff[i](p, 0) + coeff[i](p, 1) * t +
-                       coeff[i](p, 2) * pow(t, 2) + coeff[i](p, 3) * pow(t, 3);
-
-      // Publishing joint_state
-      sensor_msgs::JointState joint_state;
-      joint_state.name = {"j1", "j2", "j3", "j4", "j5", "j6", "j7"};
+    robot_->control([&](const franka::RobotState &robot_state,
+                        franka::Duration period) -> franka::JointPositions {
+      // Publishing current joint_state
+      joint_state.position.clear();
       for (int i = 0; i < 7; i++) {
-        joint_state.position.push_back(q_command[i]);
+        joint_state.position.push_back(robot_state.q[i]);
       }
       joint_state.header.stamp = ros::Time::now();
       state_pub_.publish(joint_state);
 
-      // Change polynomial function: must be the last thing in the cycle
-      if (t > points[p + 1].time_from_start.toSec()) {
-        p++;
-        ROS_INFO("Switched polynomial function, t = %f \n", t);
+      // track passing of time
+      t += period.toSec();
+      joint_traj_feedback.time_left = tf - t;
+      joint_traj_as_.publishFeedback(joint_traj_feedback);
+
+      // Compute desired joint state
+      for (int joint = 0; joint < 7; joint++)
+        q_command[joint] = coeff[joint](p, 0) + coeff[joint](p, 1) * t +
+                           coeff[joint](p, 2) * pow(t, 2) +
+                           coeff[joint](p, 3) * pow(t, 3);
+
+      // Check if the action has been preempteed
+      if (joint_traj_as_.isPreemptRequested() || !ros::ok()) {
+        ROS_INFO("Preempted: Joint Trajectory \n ");
+        // set the action state to preempted
+        joint_point_traj_as_.setPreempted();
+        return franka::MotionFinished(franka::JointPositions(q_command));
       }
-      loop_rate.sleep();
+
+      // Change polynomial coefficients.
+      if (t > points[p + 1].time_from_start.toSec())
+        p++;
+
+      if (t < tf)
+        return franka::JointPositions(q_command);
+      else {
+        std::cout << "Motion finished \n";
+        success = true;
+        return franka::MotionFinished(franka::JointPositions(q_command));
+      }
+    });
+
+    if (success) {
+      joint_traj_result.success = true;
+      ROS_INFO("Succeeded: Joint Trajectory \n ");
+      // set the action state to succeeded
+      joint_traj_as_.setSucceeded(joint_traj_result);
     }
-
-    joint_traj_result_.success = true;
-    joint_traj_as_.setSucceeded(joint_traj_result_);
-
-    // robot_->control([&](const franka::RobotState &,
-    //                     franka::Duration period) -> franka::JointPositions {
-    //   t += period.toSec();
-
-    //   return franka::JointPositions(q_c);
-    // });
 
   } catch (const franka::Exception &ex) {
     // print exception
@@ -205,6 +209,14 @@ void TrajAction::JointPointTrajCB(
 
     robot_->control([&](const franka::RobotState &robot_state,
                         franka::Duration period) -> franka::JointPositions {
+      // Publishing joint_state
+      joint_state.position.clear();
+      for (int i = 0; i < 7; i++) {
+        joint_state.position.push_back(robot_state.q[i]);
+      }
+      joint_state.header.stamp = ros::Time::now();
+      state_pub_.publish(joint_state);
+
       t += period.toSec();
       double tau = t / tf;
       for (int i = 0; i < 7; i++)
@@ -229,14 +241,6 @@ void TrajAction::JointPointTrajCB(
         success = true;
         return franka::MotionFinished(franka::JointPositions(q_goal));
       }
-
-      // Publishing joint_state
-      joint_state.position.clear();
-      for (int i = 0; i < 7; i++) {
-        joint_state.position.push_back(robot_state.q[i]);
-      }
-      joint_state.header.stamp = ros::Time::now();
-      state_pub_.publish(joint_state);
     });
 
     if (success) {
@@ -247,8 +251,10 @@ void TrajAction::JointPointTrajCB(
     }
 
   } catch (const franka::Exception &ex) {
-    robot_mutex_.unlock();
+
     // print exception
     std::cout << ex.what() << std::endl;
   }
+
+  robot_mutex_.unlock();
 }
